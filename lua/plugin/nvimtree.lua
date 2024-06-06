5aa72f78fee36d1fdbc7dca42bddfdba96c47d2e
sub-kek <subbkek@gmail.com>
2024-06-06 17:25:10 +0300

    Maybe no bugs. All done)

--- lua/plugin/nvimtree.lua
+++ lua/plugin/nvimtree.lua
@@ -1,1 +1,23 @@
-require("nvim-tree").setup {}
+require("nvim-tree").setup {
+	filters = {
+    enable = false,
+  },
+	git = {
+		enable = true,
+		show_on_dirs = true,
+		show_on_open_dirs = true,
+	},
+	diagnostics = {
+		enable = true,
+    show_on_dirs = true,
+    show_on_open_dirs = true,
+    severity = {
+      min = vim.diagnostic.severity.HINT,
+      max = vim.diagnostic.severity.ERROR,
+    },
+    icons = {
+      hint = "",
+      error = "",
+    },
+	},
+}
